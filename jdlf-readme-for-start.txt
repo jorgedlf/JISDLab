ab4da6751e43972802631d8b7e3062d90762fa9b
jorge <jorgedjdlf@hotmail.com>
2025-04-29 23:15:52 -0300

    Primeros cambios.

---
+++ jdlf-readme-for-start.txt
@@ -0,0 +1,3 @@
+Ejecutar en PowerShell, antes del comando jdlf-start.ps1
+ 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Scope Process
---
+++ jdlf-start.ps1
@@ -0,0 +1,9 @@
+# Por JdlF
+# 27/4/2025
+
+# Establece la variable de entorno JISDLAB_HOME a la ruta absoluta del directorio JISDLab.
+$env:JISDLAB_HOME="C:\Users\jorge\JdlF-PG19-WDG\14\prj\JISDLab"
+
+# Correr lo siguiente en el directorio JISDLab.
+#JISD\setup\setup-windows.ps1 <your classpath>
+JISD\setup\setup-windows.ps1 "..\..\jdlf-d-p-jupyter-notebooks\jupyter-notebooks"
