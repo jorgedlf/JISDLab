8768f35d97b1444f18108a9ef294133df81bbeeb
Jorge <jorgedjdlf@hotmail.com>
2025-05-09 18:45:10 -0300

    Para tomar la ruta actual de directorios.

--- jdlf-start.ps1
+++ jdlf-start.ps1
@@ -2,7 +2,8 @@
 # 27/4/2025
 
 # Establece la variable de entorno JISDLAB_HOME a la ruta absoluta del directorio JISDLab.
-$env:JISDLAB_HOME="C:\Users\jorge\JdlF-PG19-WDG\14\prj\JISDLab"
+#$env:JISDLAB_HOME="C:\Users\jorge\JdlF-PG19-WDG\14\prj\JISDLab"
+$env:JISDLAB_HOME=Get-Location
 
 # Correr lo siguiente en el directorio JISDLab.
 #JISD\setup\setup-windows.ps1 <your classpath>
